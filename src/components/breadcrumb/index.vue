297ea136e0c24ac4a0662c28be0be299344fb09d
foolishmax <827864735@qq.com>
2022-02-18 23:54:25 +0800

    feat(breadcrumb): 添加面包屑动画

--- src/components/breadcrumb/index.vue
+++ src/components/breadcrumb/index.vue
@@ -1,23 +1,37 @@
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item
-      v-for="(item, index) in breadcrumbData"
-      :key="item.path"
-    >
-      <span v-if="index !== breadcrumbData.length - 1" class="redirect">{{
-        item.meta.title
-      }}</span>
-      <span v-else class="no-redirect">{{ item.meta.title }}</span>
-    </el-breadcrumb-item>
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item
+        v-for="(item, index) in breadcrumbData"
+        :key="item.path"
+      >
+        <span
+          v-if="index !== breadcrumbData.length - 1"
+          class="redirect"
+          @click="onBreadcrumbClick(item)"
+          >{{ item.meta.title }}</span
+        >
+        <span v-else class="no-redirect">{{ item.meta.title }}</span>
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
 
 <script setup>
 import { ref, watch } from "vue";
-import { useRoute } from "vue-router";
+import { useStore } from "vuex";
+import { useRoute, useRouter } from "vue-router";
+
+const store = useStore();
+const route = useRoute();
+const router = useRouter();
 
 const breadcrumbData = ref([]);
-const route = useRoute();
+const hoverColor = ref(store.getters.cssVar.menuBg);
+
+const onBreadcrumbClick = (item) => {
+  router.push(item.path);
+};
 
 const getBreadcrumbData = () => {
   breadcrumbData.value = route.matched.filter((item) => item.meta.title);
@@ -33,13 +47,33 @@ watch(
 </script>
 
 <style lang="scss" scoped>
+.redirect {
+  color: #666;
+  font-weight: 600;
+  cursor: pointer;
+
+  &:hover {
+    color: v-bind(hoverColor);
+  }
+}
+
 .no-redirect {
   color: #97a8be;
   cursor: text;
 }
 
-.redirect {
-  color: #666;
-  font-weight: 600;
+.breadcrumb-enter-active,
+.breadcrumb-leave-active {
+  transition: all 0.5s;
+}
+
+.breadcrumb-enter-from,
+.breadcrumb-leave-to {
+  opacity: 0;
+  transform: translateX(20px);
+}
+
+.breadcrumb-leave-active {
+  position: absolute;
 }
 </style>
